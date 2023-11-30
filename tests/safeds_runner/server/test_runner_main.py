01df8891985b240ebd1ed2f1560f0cdacb1f6a55
WinPlay02 <winplay02_gh@woberlaender.de>
2023-11-30 21:36:52 +0100

    fix: runner startup crash (#15)
    ### Summary of Changes
    
    - fixes a crash on startup as the multiprocessing manager causes a new
    process to spawn during the bootstrap phase
    - this error only occurred during execution of the main.py with a new
    interpreter
    
    ---------
    
    Co-authored-by: megalinter-bot <129584137+megalinter-bot@users.noreply.github.com>
    Co-authored-by: Lars Reimann <mail@larsreimann.com>

---
+++ tests/safeds_runner/server/test_runner_main.py
@@ -0,0 +1,17 @@
+import subprocess
+import typing
+from pathlib import Path
+from typing import IO
+
+_project_root: Path = Path(__file__).parent / ".." / ".." / ".."
+
+
+def test_should_runner_start_successfully() -> None:
+    process = subprocess.Popen(["poetry", "run", "safe-ds-runner"], cwd=_project_root, stderr=subprocess.PIPE)
+    while process.poll() is None:
+        process_line = str(typing.cast(IO[bytes], process.stderr).readline(), "utf-8").strip()
+        # Wait for first line of log
+        if process_line.startswith("INFO:root:Starting Safe-DS Runner"):
+            process.kill()
+            return
+    assert process.poll() == 0
