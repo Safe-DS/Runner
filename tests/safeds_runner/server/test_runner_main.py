e42910250030a730ff728e92102e884ba1b40457
WinPlay02 <winplay02_gh@woberlaender.de>
2023-11-30 20:00:21 +0100

    test: successful startup of runner process

---
+++ tests/safeds_runner/server/test_runner_main.py
@@ -0,0 +1,15 @@
+import subprocess
+from pathlib import Path
+
+_project_root: Path = Path(__file__).parent / ".." / ".." / ".."
+
+
+def test_should_runner_start_successfully() -> None:
+    process = subprocess.Popen(["poetry", "run", "safe-ds-runner"], cwd=_project_root, stderr=subprocess.PIPE)
+    while process.poll() is None:
+        process_line = str(process.stderr.readline(), "utf-8").strip()
+        # Wait for first line of log
+        if process_line.startswith("INFO:root:Starting Safe-DS Runner"):
+            process.kill()
+            return
+    assert process.poll() == 0
