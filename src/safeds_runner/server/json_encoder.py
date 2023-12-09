054cca4cf8025932c0a73e1f734a31fb20cab99a
WinPlay02 <winplay02_gh@woberlaender.de>
2023-12-09 09:42:54 +0100

    feat: added json serializer that encodes tables and images (#29)
    Closes #20 
    
    ### Summary of Changes
    
    - added SafeDSEncoder that is used for custom types (tables, images)
    - don't crash when unknown types are attempted to be sent, instead send
    a "\<Not Displayable\>" placeholder value
    - added encoding tests + not displayable to existing message exchange
    
    ---------
    
    Co-authored-by: megalinter-bot <129584137+megalinter-bot@users.noreply.github.com>

---
+++ src/safeds_runner/server/json_encoder.py
@@ -0,0 +1,51 @@
+"""Module containing JSON encoding utilities for Safe-DS types."""
+
+import base64
+import json
+import math
+from typing import Any
+
+from safeds.data.image.containers import Image
+from safeds.data.image.typing import ImageFormat
+from safeds.data.tabular.containers import Table
+
+
+class SafeDsEncoder(json.JSONEncoder):
+    """JSON Encoder for custom Safe-DS types."""
+
+    def default(self, o: Any) -> Any:
+        """
+        Convert specific Safe-DS types to a JSON-serializable representation.
+
+        If values are custom Safe-DS types (such as Table or Image) they are converted to a serializable representation.
+        If a value is not handled here, the default encoding implementation is called.
+        In case of Tables, note that NaN values are converted to JSON null values.
+
+        Parameters
+        ----------
+        o: Any
+            An object that needs to be encoded to JSON.
+
+        Returns
+        -------
+        Any
+            The passed object represented in a way that is serializable to JSON.
+        """
+        if isinstance(o, Table):
+            dict_with_nan_infinity = o.to_dict()
+            # Convert NaN / Infinity to None, as the JSON encoder generates invalid JSON otherwise
+            return {
+                key: [
+                    value if not isinstance(value, float) or math.isfinite(value) else None
+                    for value in dict_with_nan_infinity[key]
+                ]
+                for key in dict_with_nan_infinity
+            }
+        if isinstance(o, Image):
+            # Send images together with their format
+            match o.format:
+                case ImageFormat.JPEG:
+                    return {"format": o.format.value, "bytes": str(base64.encodebytes(o._repr_jpeg_()), "utf-8")}
+                case ImageFormat.PNG:
+                    return {"format": o.format.value, "bytes": str(base64.encodebytes(o._repr_png_()), "utf-8")}
+        return json.JSONEncoder.default(self, o)
