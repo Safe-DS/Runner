9e86acdcac496a2c1fb0800feaf6ed91717f76b1
WinPlay02 <winplay02_gh@woberlaender.de>
2024-01-13 00:55:11 +0100

    test: just import SafeDsServer once + re-enable temporary disabled timeout

--- tests/safeds_runner/server/test_websocket_mock.py
+++ tests/safeds_runner/server/test_websocket_mock.py
@@ -20,6 +20,7 @@ from safeds_runner.server.messages import (
     message_type_runtime_progress,
 )
 from safeds_runner.server.pipeline_manager import PipelineManager
+from safeds_runner.server.server import SafeDsServer
 
 app_pipeline_manager = PipelineManager()
 
@@ -176,8 +177,6 @@ class MockWebsocketConnection:
 def test_should_fail_message_validation(websocket_message: str, exception_message: str) -> None:
     mock_connection = MockWebsocketConnection([websocket_message])
     app_pipeline_manager.connect(mock_connection)
-    from safeds_runner.server.server import SafeDsServer
-
     SafeDsServer._ws_main(mock_connection, app_pipeline_manager)
     assert str(mock_connection.close_message) == exception_message
 
@@ -221,8 +220,6 @@ def test_should_execute_pipeline_return_exception(
 ) -> None:
     mock_connection = MockWebsocketConnection(messages)
     app_pipeline_manager.connect(mock_connection)
-    from safeds_runner.server.server import SafeDsServer
-
     SafeDsServer._ws_main(mock_connection, app_pipeline_manager)
     mock_connection.wait_for_messages(1)
     exception_message = Message.from_dict(json.loads(mock_connection.get_next_received_message()))
@@ -312,8 +309,6 @@ def test_should_execute_pipeline_return_valid_placeholder(
     # Initial execution
     mock_connection = MockWebsocketConnection(initial_messages)
     app_pipeline_manager.connect(mock_connection)
-    from safeds_runner.server.server import SafeDsServer
-
     SafeDsServer._ws_main(mock_connection, app_pipeline_manager)
     # Wait for at least enough messages to successfully execute pipeline
     mock_connection.wait_for_messages(initial_execution_message_wait)
@@ -389,8 +384,6 @@ def test_should_execute_pipeline_return_valid_placeholder(
 def test_should_successfully_execute_simple_flow(messages: list[str], expected_response: Message) -> None:
     mock_connection = MockWebsocketConnection(messages)
     app_pipeline_manager.connect(mock_connection)
-    from safeds_runner.server.server import SafeDsServer
-
     SafeDsServer._ws_main(mock_connection, app_pipeline_manager)
     mock_connection.wait_for_messages(1)
     query_result_invalid = Message.from_dict(json.loads(mock_connection.get_next_received_message()))
@@ -424,12 +417,10 @@ def helper_should_shut_itself_down_run_in_subprocess(sub_messages: list[str]) ->
     mock_connection = MockWebsocketConnection(sub_messages)
     pipeline_manager = PipelineManager()
     pipeline_manager.startup()
-    from safeds_runner.server.server import SafeDsServer
-
     SafeDsServer._ws_main(mock_connection, pipeline_manager)
 
 
-# @pytest.mark.timeout(45)
+@pytest.mark.timeout(45)
 def test_should_accept_at_least_2_parallel_connections_in_subprocess() -> None:
     port = 6000
     server_output_pipes_stderr_r, server_output_pipes_stderr_w = multiprocessing.Pipe()
