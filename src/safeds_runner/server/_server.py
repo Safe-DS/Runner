ad216d85c76dc2ea4339615061eb36e3aac1316b
Lars Reimann <mail@larsreimann.com>
2024-04-29 22:44:01 +0200

    fix: arbitrary args for payload of shutdown/catch all

--- src/safeds_runner/server/_server.py
+++ src/safeds_runner/server/_server.py
@@ -121,12 +121,11 @@ class SafeDsServer:
         #     await sio.emit(message_type_placeholder_value, data, to=placeholder_query_message.id)
 
         @sio.event
-        async def shutdown(_sid: str, _payload: Any = None) -> None:
+        async def shutdown(_sid: str, *_args: Any) -> None:
             logging.info("Shutting down...")
             await self.shutdown()
             sys.exit(0)
 
         @sio.on("*")
-        async def catch_all(event: str) -> str:
+        async def catch_all(event: str, *_args: Any) -> None:
             logging.exception("Invalid message type: %s", event)
-            return "Invalid message type"
