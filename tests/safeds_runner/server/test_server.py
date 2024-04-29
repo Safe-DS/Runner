748ae0ccba3976c1d473ff1fcdbecf9d44714446
Lars Reimann <mail@larsreimann.com>
2024-04-29 19:43:07 +0200

    test: shutdown message

--- tests/safeds_runner/server/test_server.py
+++ tests/safeds_runner/server/test_server.py
@@ -2,8 +2,10 @@ from __future__ import annotations
 
 import asyncio
 import json
+import multiprocessing
 import threading
 
+import psutil
 import pytest
 import socketio
 from pydantic import ValidationError
@@ -19,8 +21,10 @@ from safeds_runner.server.messages._to_server import (
     MessageToServer,
     VirtualModule,
     create_run_message,
+    create_shutdown_message,
 )
 
+BASE_TIMEOUT = 10
 PORT = 17394
 URL = f"http://localhost:{PORT}"
 
@@ -50,6 +54,8 @@ async def client() -> socketio.AsyncSimpleClient:
         await sio.connect(URL, transports=["websocket"])
         yield sio
 
+
+# Test runtime warning -------------------------------------------------------------------------------------------------
 
 @pytest.mark.parametrize(
     argnames=("request_", "expected_response"),
@@ -85,7 +91,7 @@ async def test_runtime_warning(
     expected_response: MessageFromServer,
 ) -> None:
     await client.emit(request_.event, request_.payload.model_dump_json())
-    [actual_event, actual_payload] = await client.receive(timeout=5)
+    [actual_event, actual_payload] = await client.receive(timeout=BASE_TIMEOUT)
 
     # Event should be correct
     assert actual_event == expected_response.event
@@ -94,7 +100,7 @@ async def test_runtime_warning(
     try:
         runtime_warning_payload = RuntimeWarningMessagePayload(**json.loads(actual_payload))
     except (TypeError, ValidationError):
-        pytest.fail("Invalid response payload")
+        pytest.fail("Invalid response payload.")
 
     # Stacktrace should not be empty
     assert len(runtime_warning_payload.stacktrace) > 0
@@ -103,6 +109,8 @@ async def test_runtime_warning(
     # Check the rest of the data
     assert runtime_warning_payload == expected_response.payload
 
+
+# Test runtime_error ---------------------------------------------------------------------------------------------------
 
 @pytest.mark.parametrize(
     argnames=("request_", "expected_response"),
@@ -137,7 +145,7 @@ async def test_runtime_error(
     expected_response: MessageFromServer,
 ) -> None:
     await client.emit(request_.event, request_.payload.model_dump_json())
-    [actual_event, actual_payload] = await client.receive(timeout=5)
+    [actual_event, actual_payload] = await client.receive(timeout=BASE_TIMEOUT)
 
     # Event should be correct
     assert actual_event == expected_response.event
@@ -146,7 +154,7 @@ async def test_runtime_error(
     try:
         runtime_error_payload = RuntimeErrorMessagePayload(**json.loads(actual_payload))
     except (TypeError, ValidationError):
-        pytest.fail("Invalid response payload")
+        pytest.fail("Invalid response payload.")
 
     # Stacktrace should not be empty
     assert len(runtime_error_payload.stacktrace) > 0
@@ -154,3 +162,45 @@ async def test_runtime_error(
 
     # Check the rest of the data
     assert runtime_error_payload == expected_response.payload
+
+
+# Test shutdown --------------------------------------------------------------------------------------------------------
+
+SHUTDOWN_PORT = PORT + 1
+SHUTDOWN_URL = f"http://localhost:{SHUTDOWN_PORT}"
+
+
+def test_shutdown() -> None:
+    process = multiprocessing.Process(target=sync_run_shutdown_server)
+    process.start()
+    process.join(3 * BASE_TIMEOUT)
+
+    # Kill the server if it did not shut down in time
+    if process.is_alive():
+        parent = psutil.Process(process.pid)
+        for child in parent.children(recursive=True):
+            child.kill()
+        pytest.fail("Server did not shut down in time.")
+
+    assert process.exitcode == 0
+
+
+def sync_run_shutdown_server():
+    asyncio.run(run_shutdown_server())
+
+
+async def run_shutdown_server():
+    # Start the server
+    server = SafeDsServer()
+    server._sio.eio.start_service_task = False
+
+    def run_server():
+        asyncio.run(server.startup(SHUTDOWN_PORT))
+
+    thread = threading.Thread(target=run_server, daemon=True)
+    thread.start()
+
+    # Connect with a client and send a shutdown message
+    async with socketio.AsyncSimpleClient() as shutdown_client:
+        await shutdown_client.connect(SHUTDOWN_URL, transports=["websocket"])
+        await shutdown_client.emit(create_shutdown_message().event)
