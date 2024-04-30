74e89cefde148d00ecc47260b62a4c2f564b7c05
Lars Reimann <mail@larsreimann.com>
2024-04-30 19:04:08 +0200

    docs: document new messages

--- src/safeds_runner/server/messages/_from_server.py
+++ src/safeds_runner/server/messages/_from_server.py
@@ -6,6 +6,17 @@ from pydantic import BaseModel, ConfigDict
 
 
 class MessageFromServer(BaseModel):
+    """
+    Message sent from the server to the client.
+
+    Attributes
+    ----------
+    event:
+        Event type of the message.
+    payload:
+        Payload of the message.
+    """
+
     event: str
     payload: MessageFromServerPayload
 
@@ -13,10 +24,36 @@ class MessageFromServer(BaseModel):
 
 
 class MessageFromServerPayload(BaseModel, ABC):
+    """
+    Base class for payloads of messages sent from the server to the client.
+
+    Attributes
+    ----------
+    run_id:
+        Identifier for the program run.
+    """
+
     run_id: str
 
 
 class PlaceholderValueMessagePayload(MessageFromServerPayload):
+    """
+    Payload for a 'placeholder_value' message.
+
+    Attributes
+    ----------
+    run_id:
+        Identifier for the program run.
+    placeholder_name:
+        Name of the placeholder.
+    type:
+        Type of the placeholder.
+    value:
+        Value of the placeholder.
+    window:
+        Window of the full value included as value in the message.
+    """
+
     run_id: str
     placeholder_name: str
     type: str
@@ -27,14 +64,40 @@ class PlaceholderValueMessagePayload(MessageFromServerPayload):
 
 
 class Window(BaseModel):
+    """
+    Window of a placeholder value. A window with start=0 and size=full_size is equivalent to the full value.
+
+    Attributes
+    ----------
+    start:
+        Start index of the window.
+    size:
+        Size of the window.
+    full_size:
+       Size of the full value.
+    """
+
     start: int
     size: int
-    max: int
+    full_size: int
 
     model_config = ConfigDict(extra="forbid")
 
 
 class RuntimeWarningMessagePayload(MessageFromServerPayload):
+    """
+    Payload for a 'runtime_warning' message.
+
+    Attributes
+    ----------
+    run_id:
+        Identifier for the program run.
+    message:
+        Warning message.
+    stacktrace:
+        Stacktrace of the warning. Entries closest to the source of the warning come first.
+    """
+
     run_id: str
     message: str
     stacktrace: list[StacktraceEntry]
@@ -43,6 +106,19 @@ class RuntimeWarningMessagePayload(MessageFromServerPayload):
 
 
 class RuntimeErrorMessagePayload(MessageFromServerPayload):
+    """
+    Payload for a 'runtime_error' message.
+
+    Attributes
+    ----------
+    run_id:
+        Identifier for the program run.
+    message:
+        Error message.
+    stacktrace:
+        Stacktrace of the error. Entries closest to the source of the error come first.
+    """
+
     run_id: str
     message: str
     stacktrace: list[StacktraceEntry]
@@ -51,6 +127,17 @@ class RuntimeErrorMessagePayload(MessageFromServerPayload):
 
 
 class StacktraceEntry(BaseModel):
+    """
+    Entry in a stacktrace. Python provides no column information, so only the file and line are available.
+
+    Attributes
+    ----------
+    file:
+        File where the error occurred.
+    line:
+        Line number where the error occurred.
+    """
+
     file: str
     line: int
 
@@ -58,6 +145,21 @@ class StacktraceEntry(BaseModel):
 
 
 class ProgressMessagePayload(MessageFromServerPayload):
+    """
+    Payload for a 'progress' message.
+
+    Attributes
+    ----------
+    run_id:
+        Identifier for the program run.
+    placeholder_name:
+        Name of the placeholder.
+    percentage:
+        Percentage of completion in the range [0, 100].
+    message:
+        Optional message to be displayed.
+    """
+
     run_id: str
     placeholder_name: str
     percentage: int
@@ -67,6 +169,15 @@ class ProgressMessagePayload(MessageFromServerPayload):
 
 
 class DoneMessagePayload(MessageFromServerPayload):
+    """
+    Payload for a 'done' message.
+
+    Attributes
+    ----------
+    run_id:
+        Identifier for the program run.
+    """
+
     run_id: str
 
     model_config = ConfigDict(extra="forbid")
@@ -79,6 +190,7 @@ def create_placeholder_value_message(
     value: str,
     window: Window | None = None,
 ) -> MessageFromServer:
+    """Create a 'placeholder_value' message."""
     return MessageFromServer(
         event="placeholder_value",
         payload=PlaceholderValueMessagePayload(
@@ -96,6 +208,7 @@ def create_runtime_warning_message(
     message: str,
     stacktrace: list[StacktraceEntry],
 ) -> MessageFromServer:
+    """Create a 'runtime_warning' message."""
     return MessageFromServer(
         event="runtime_warning",
         payload=RuntimeWarningMessagePayload(run_id=run_id, message=message, stacktrace=stacktrace),
@@ -103,6 +216,7 @@ def create_runtime_warning_message(
 
 
 def create_runtime_error_message(run_id: str, message: str, stacktrace: list[StacktraceEntry]) -> MessageFromServer:
+    """Create a 'runtime_error' message."""
     return MessageFromServer(
         event="runtime_error",
         payload=RuntimeErrorMessagePayload(run_id=run_id, message=message, stacktrace=stacktrace),
@@ -115,6 +229,7 @@ def create_progress_message(
     percentage: int,
     message: str | None = None,
 ) -> MessageFromServer:
+    """Create a 'progress' message."""
     return MessageFromServer(
         event="progress",
         payload=ProgressMessagePayload(
@@ -127,4 +242,5 @@ def create_progress_message(
 
 
 def create_done_message(run_id: str) -> MessageFromServer:
+    """Create a 'done' message."""
     return MessageFromServer(event="done", payload=DoneMessagePayload(run_id=run_id))
